48cf03cfe916d837356e297eff7605a373f392c5
Chanakya <sunkarapallychanakya@gmail.com>
2022-07-23 20:29:53 +0530

    ft: initial commit, backtracking problem

---
+++ recursion_reverse_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void swap(int*arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void reverseArray(int *arr, int i, int j) {
+    if(i>=j) {
+        return;
+    }
+    swap(arr, i, j);
+    reverseArray(arr, i+1, j-1);
+}
+
+int main() {
+    int arr[] = {4,5,1,2,3,4};
+    int n = sizeof(arr)/sizeof(int);
+    reverseArray(arr, 0,n - 1);
+    for(int i=0; i<n;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+    return 0;
+}
